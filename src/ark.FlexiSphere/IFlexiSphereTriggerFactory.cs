8bd4ce6f84a68d4755ecd937fe9221578541335b
jmrpineda <jmr.pineda@gmail.com>
2025-02-05 06:34:03 +0100

    Moving a more accurated namespace

---
+++ src/ark.FlexiSphere/IFlexiSphereTriggerFactory.cs
@@ -0,0 +1,89 @@
+#region Header
+
+// --------------------------------------------------------------------------------------
+// Powered by:
+// 
+//     __________.__                  .___    ___________                             
+//     \______   \__| ____   ____   __| _/____\__    ___/___   ____       ____  __ __ 
+//      |     ___/  |/    \_/ __ \ / __ |\__  \ |    |_/ __ \_/ ___\    _/ __ \|  |  \
+//      |    |   |  |   |  \  ___// /_/ | / __ \|    |\  ___/\  \___    \  ___/|  |  /
+//      |____|   |__|___|  /\___  >____ |(____  /____| \___  >\___  > /\ \___  >____/ 
+//                   \/     \/     \/     \/           \/     \/  \/     \/
+// 
+// 
+// FileName: IFlexiSphereTriggerFactory.cs
+//
+// Author:   jmr.pineda
+// eMail:    jmr.pineda@pinedatec.eu
+// Profile:  http://pinedatec.eu/profile
+//
+//           Copyrights (c) PinedaTec.eu 2025, all rights reserved.
+//           CC BY-NC-ND - https://creativecommons.org/licenses/by-nc-nd/4.0
+//
+//  Created at: 2025-02-04T13:18:01.092Z
+//
+// --------------------------------------------------------------------------------------
+
+#endregion
+
+namespace ark.FlexiSphere;
+
+public interface IFlexiSphereTriggerFactory
+{
+    /// <summary>
+    /// Set the owener for the trigger
+    /// </summary>
+    /// <param name="owner"></param>
+    /// <returns></returns>
+    IFlexiSphereTriggerFactory SetOwner(IFlexiSphereFactory owner);
+
+    /// <summary>
+    /// Set the trigger name and group
+    /// </summary>
+    /// <param name="triggerName"></param>
+    /// <param name="triggerGroup"></param>
+    /// <returns></returns>
+    IFlexiSphereTriggerFactory WithTriggerName(string triggerName, string triggerGroup);
+
+    /// <summary>
+    /// Set the max occurences for the trigger
+    /// </summary>
+    /// <param name="maxOccurences"></param>
+    /// <returns></returns>
+    IFlexiSphereTriggerFactory SetMaxOccurences(int maxOccurences);
+
+    /// <summary>
+    /// Set the max concurrent for the trigger
+    /// </summary>
+    /// <param name="maxConcurrent"></param>
+    /// <returns></returns> 
+    IFlexiSphereTriggerFactory SetMaxConcurrent(int maxConcurrent);
+
+    /// <summary>
+    /// Define the action to be executed <c ref="FlexiSphereEventTrigger" />
+    /// </summary>
+    /// <param name="eventAction"></param>
+    /// <param name="delay"></param>
+    /// <returns></returns>
+    IFlexiSphereTriggerFactory ActivateOnAction(Func<IFlexiSphereContext?, Task<bool>> eventAction, int delay = 1000);
+
+    /// <summary>
+    /// Define the cron expression for the trigger <c ref="FlexiSphereScheduledTrigger" />
+    /// </summary>
+    /// <param name="cronExpression"></param>
+    /// <returns></returns>
+    IFlexiSphereTriggerFactory StartOn(string cronExpression);
+
+    /// <summary>
+    /// Allow the trigger to fire on start
+    /// </summary>
+    /// <param name="activate"></param>
+    /// <returns></returns>
+    IFlexiSphereTriggerFactory FireTriggerOnStart(bool activate);
+
+    /// <summary>
+    /// Build the trigger
+    /// </summary>
+    /// <returns></returns>
+    IFlexiSphereTrigger Build();
+}
---
+++ src/ark.FlexiSphere/triggers/FlexiSphereEventTrigger.cs
@@ -0,0 +1,98 @@
+#region Header
+
+// --------------------------------------------------------------------------------------
+// Powered by:
+// 
+//     __________.__                  .___    ___________                             
+//     \______   \__| ____   ____   __| _/____\__    ___/___   ____       ____  __ __ 
+//      |     ___/  |/    \_/ __ \ / __ |\__  \ |    |_/ __ \_/ ___\    _/ __ \|  |  \
+//      |    |   |  |   |  \  ___// /_/ | / __ \|    |\  ___/\  \___    \  ___/|  |  /
+//      |____|   |__|___|  /\___  >____ |(____  /____| \___  >\___  > /\ \___  >____/ 
+//                   \/     \/     \/     \/           \/     \/  \/     \/
+// 
+// 
+// FileName: FlexiSphereEventTrigger.cs
+//
+// Author:   jmr.pineda
+// eMail:    jmr.pineda@pinedatec.eu
+// Profile:  http://pinedatec.eu/profile
+//
+//           Copyrights (c) PinedaTec.eu 2025, all rights reserved.
+//           CC BY-NC-ND - https://creativecommons.org/licenses/by-nc-nd/4.0
+//
+//  Created at: 2025-02-04T13:18:01.093Z
+//
+// --------------------------------------------------------------------------------------
+
+#endregion
+
+using ark.extensions;
+using NLog;
+
+namespace ark.FlexiSphere.triggers;
+
+public class FlexiSphereEventTrigger : FlexiSphereTriggerBase, IFlexiSphereEventTrigger
+{
+    private Func<IFlexiSphereContext?, Task<bool>>? _eventAction;
+
+    private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
+    public void ConfigureTrigger(Func<IFlexiSphereContext?, Task<bool>> eventAction, int delay = 1000,
+        int maxConcurrent = 1, int maxOccurences = 0)
+    {
+        base.ConfigureTrigger(delay, maxConcurrent, maxOccurences);
+
+        _eventAction = eventAction;
+    }
+
+    public override void ActivateTrigger(IFlexiSphereContext? context = null, CancellationToken cancellationToken = default)
+    {
+        base.ActivateTrigger(context, cancellationToken);
+
+        _eventAction.ThrowExceptionIfNull($"{nameof(_eventAction)} cannot be null!");
+    }
+
+
+    protected override async Task TimerCallback(object? state)
+    {
+        try
+        {
+            var cancellationToken = (CancellationToken)state!;
+            if (cancellationToken.IsCancellationRequested)
+            {
+                this.DeactivateTrigger("Cancellation requested!");
+
+                base.RaiseOnFlexiSphereTriggerCanceled(base._context);
+                return;
+            }
+
+            if (this.MaxOccurrences > 0 && this.Counter >= this.MaxOccurrences)
+            {
+                this.DeactivateTrigger("Max occurrences reached!");
+
+                base.RaiseOnFlexiSphereTriggerCompleted(base._context);
+                return;
+            }
+
+            _logger.Info($"TimerCallback: {0} occurrences", _semaphore!.CurrentCount);
+
+            await _semaphore!.WaitAsync();
+
+            base.IncrementCounter();
+            var result = await _eventAction!.Invoke(base._context);
+
+            if (result)
+            {
+                base.RaiseOnFlexiSphereTriggered(base._context);
+            }
+        }
+        catch (Exception ex)
+        {
+            base.RaiseOnFlexiSphereTriggerFaulted(base._context, ex);
+        }
+        finally
+        {
+            _semaphore!.Release();
+        }
+    }
+}
